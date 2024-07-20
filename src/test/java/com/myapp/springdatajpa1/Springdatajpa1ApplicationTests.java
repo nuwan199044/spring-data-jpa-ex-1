9bb223d02ee2245f65f5567ee341b4baf2cdb81a
Nuwan <nuwan199044@gmail.com>
2024-07-21 00:29:10 +0530

    initial code

---
+++ src/test/java/com/myapp/springdatajpa1/Springdatajpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springdatajpa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springdatajpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
