4775e7dd2565d370255d72fd28ec4538d9c5fdff
Nuwan <nuwan199044@gmail.com>
2024-07-21 21:01:20 +0530

    usage of @Entity, @Id, @GeneratedValue, @Column and @Enumerated

---
+++ src/main/java/com/myapp/springdatajpa1/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.myapp.springdatajpa1.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String identifier;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private LocalDate birthDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EmployeeRole role;
+}
---
+++ src/main/java/com/myapp/springdatajpa1/entity/EmployeeRole.java
@@ -0,0 +1,5 @@
+package com.myapp.springdatajpa1.entity;
+
+public enum EmployeeRole {
+    ROLE_ADMIN, ROLE_USER
+}
